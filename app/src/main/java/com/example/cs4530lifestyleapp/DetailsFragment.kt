c014b010b40df40c50b8d6624f0a03a16648dde8
realloganholmes <logan.holmes@utah.edu>
2023-02-07 09:49:42 -0700

    Fleshed out the form and added TODO's

--- app/src/main/java/com/example/cs4530lifestyleapp/DetailsFragment.kt
+++ app/src/main/java/com/example/cs4530lifestyleapp/DetailsFragment.kt
@@ -1,22 +1,24 @@
 package com.example.cs4530lifestyleapp
 
 import android.content.Context
-import android.widget.EditText
 import com.example.cs4530lifestyleapp.SubmitFragment.DataPassingInterface
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.*
 import com.example.cs4530lifestyleapp.R
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import java.lang.ClassCastException
 
+
 class SubmitFragment : Fragment(), View.OnClickListener {
+    // TODO: create variables for all form elements
     private var mEtFullName: EditText? = null
     private var mBtSubmit: Button? = null
     private var mStringFullName: String? = null
+    private var spActivityLevel: Spinner? = null
+
     var mDataPasser: DataPassingInterface? = null
 
     //Callback interface
@@ -41,16 +43,29 @@ class SubmitFragment : Fragment(), View.OnClickListener {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_details, container, false)
 
-        //Get stuff
+        //TODO: get all form elements from the view
         mEtFullName = view.findViewById(R.id.et_fullname) as EditText
         mBtSubmit = view.findViewById(R.id.button_submit) as Button
+        spActivityLevel = view.findViewById(R.id.spActivityLevel) as Spinner
         mBtSubmit!!.setOnClickListener(this)
+
+        // Add spinner array to activity level spinner
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.array_activity_level,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spActivityLevel!!.adapter = adapter
+        }
+
         return view
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.button_submit -> {
+                // TODO: Get all other data from the form. This only gets the name field.
                 mStringFullName = mEtFullName!!.text.toString()
 
                 //Check if the EditText string is empty
@@ -74,6 +89,7 @@ class SubmitFragment : Fragment(), View.OnClickListener {
                             Toast.LENGTH_SHORT
                         ).show()
                     } else if (splitStrings.size == 2) {
+                        // TODO: When calling passdata method, we need to pass all of the form data, not just the name
                         mDataPasser!!.passData(splitStrings)
                     } else {
                         Toast.makeText(
--- app/src/main/java/com/example/cs4530lifestyleapp/MainActivity.kt
+++ app/src/main/java/com/example/cs4530lifestyleapp/MainActivity.kt
@@ -8,16 +8,27 @@ import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity(), DataPassingInterface, View.OnClickListener {
-    private var mStringFirstName: String? = null
-    private var mStringLastName: String? = null
+    // View Elements
+    // TODO: instantiate the rest of the view elements
     private var mTvFirstName: TextView? = null
     private var mTvLastName: TextView? = null
     private var btnEditDetails: Button? = null
+
+    // Variables to store data in so we can keep it.
+    private var mStringFirstName: String? = null
+    private var mStringLastName: String? = null
+    private var weight: Int? = null
+    private var height: Int? = null
+    private var activityLevel: String? = null
+    private var age: Int? = null
+    private var location: String? = null
+    private var sex: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Get stuff
+        // TODO: get rest of view elements
         mTvFirstName = findViewById<View>(R.id.tv_fn_data) as TextView
         mTvLastName = findViewById<View>(R.id.tv_ln_data) as TextView
         btnEditDetails = findViewById<View>(R.id.btnEditDetails) as Button
@@ -26,6 +37,7 @@ class MainActivity : AppCompatActivity(), DataPassingInterface, View.OnClickList
     }
 
     override fun passData(data: Array<String?>?) {
+        // TODO: read all form data, not just the name. Save data to local variables like we do with the name already
         mStringFirstName = data!![0]
         mStringLastName = data[1]
         if (mStringFirstName != null) {
@@ -34,6 +46,8 @@ class MainActivity : AppCompatActivity(), DataPassingInterface, View.OnClickList
         if (mTvLastName != null) {
             mTvLastName!!.text = mStringLastName
         }
+
+        // Kill fragment
         getSupportFragmentManager().findFragmentByTag("submit_frag")?.let {
             getSupportFragmentManager().beginTransaction().remove(it).commit()
         };
@@ -47,6 +61,7 @@ class MainActivity : AppCompatActivity(), DataPassingInterface, View.OnClickList
                 val submitFragment = SubmitFragment()
 
                 //Replace the fragment container
+                // TODO: we should pass the fragment all the data the user has already input so that if they just want to edit one part they dont have to reenter everything
                 val fTrans = supportFragmentManager.beginTransaction()
                 fTrans.replace(R.id.fl_frag_container, submitFragment, "submit_frag")
                 fTrans.commit()
