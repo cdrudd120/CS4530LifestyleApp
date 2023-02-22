16abc4044fc3ed93f491e3fb8df0ad63b1efd23b
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 <haydnthurman@yahoo.com>
2023-02-22 12:43:48 -0700

    Displays hikes around the user's precise location, instead of a hard-coded location.

--- app/src/main/java/com/example/cs4530lifestyleapp/MainActivity.kt
+++ app/src/main/java/com/example/cs4530lifestyleapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.cs4530lifestyleapp
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cs4530lifestyleapp.DetailsFragment.DetailsPassing
@@ -9,11 +11,15 @@ import com.example.cs4530lifestyleapp.ListFragment.ListPassing
 import com.example.cs4530lifestyleapp.ShowDetailsFragment.ShowDetailsPassing
 import com.example.cs4530lifestyleapp.WeatherFragment.WeatherPassing
 import com.example.cs4530lifestyleapp.BMIFragment.BMIPassing
+import com.google.android.gms.location.FusedLocationProviderClient
 
 import android.widget.TextView
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity(), DetailsPassing, ListPassing, ShowDetailsPassing, WeatherPassing, BMIPassing {
     // Variables to store data in so we can keep it.
@@ -25,10 +31,14 @@ class MainActivity : AppCompatActivity(), DetailsPassing, ListPassing, ShowDetai
     private var mAge: String? = null
     private var mLocation: String? = null
     private var mSex: String? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var latitude: String? = null
+    private var longtitude: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         displayButtonFragment()
     }
@@ -86,7 +96,10 @@ class MainActivity : AppCompatActivity(), DetailsPassing, ListPassing, ShowDetai
     }
 
     private fun displayHikes() {
-        val searchUri = Uri.parse("geo:40.767778,-111.845205?q=hikes")
+        getLocation()
+
+        val longAndLatString = "geo:$longtitude,$latitude?q=hikes"
+        val searchUri = Uri.parse(longAndLatString)
         //Create the implicit intent
         val mapIntent = Intent(Intent.ACTION_VIEW, searchUri)
         //If there's an activity associated with this intent, launch it
@@ -94,6 +107,29 @@ class MainActivity : AppCompatActivity(), DetailsPassing, ListPassing, ShowDetai
             startActivity(mapIntent)
         }catch(ex: ActivityNotFoundException){
             //handle errors here
+        }
+    }
+
+    private fun getLocation() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            !=PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "We need location permission.", Toast.LENGTH_SHORT)
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 100)
+            return
+        }
+
+        val location = fusedLocationProviderClient.lastLocation
+        location.addOnSuccessListener {
+            if(it == null) {
+                Toast.makeText(this, "Sorry, can't get location.", Toast.LENGTH_SHORT)
+            }
+            else {
+                latitude = it.latitude.toString()
+                longtitude = it.longitude.toString()
+            }
+
         }
     }
 
